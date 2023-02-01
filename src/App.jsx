e5284dd401e328d36cb41cc67731fdbe15ee5a18
Himanshu <kumarhimanshu2219@gmail.com>
2023-02-01 13:06:43 +0530

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import React,{useState, createContext} from "react";
-import CompA from "./components/CompA";
+import React,{useReducer} from "react";
 
-const CounterContext = createContext();
+const reducer = (state,action) => {
+    if(action.type==="INC"){
+        return state+2
+    }else if(action.type==="DEC"){
+        return state-2;
+    }else if(action.type==="MUL"){
+        return state*2
+    }
+    return state;
+}
+
+let initialState = 0;
 
 const App = () => {
-  const [state,setState] = useState(0);
-
-  return (
-    <>
-    <CounterContext.Provider value={{state,setState}}>
-       APP 
-       <CompA/>
-      {/* <button onClick={()=>setState(state+1)}>Click here to increase</button> */}
-    </CounterContext.Provider>
-    </>
-  )
+    const [state,dispatch] = useReducer(reducer,initialState)
+    return (
+        <>
+            <h1>USE REDUCER</h1>
+            <h1>{state}</h1>
+            <button onClick={()=>dispatch({type:'INC'})}>Increment</button>
+            <button onClick={()=>{dispatch({type:'DEC'})}}>Decrement</button>
+            <button onClick={()=>{dispatch({type:'MUL'})}}>Multiplication</button>
+        </>
+    )
 }
 
 export default App;
-export {CounterContext}
