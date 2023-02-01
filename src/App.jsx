0d957de8517f79fb8ee75a5c14ff22892adb3339
Himanshu <kumarhimanshu2219@gmail.com>
2023-02-01 12:49:33 +0530

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useState, createContext} from "react";
+import CompA from "./components/CompA";
+
+const CounterContext = createContext();
 
 const App = () => {
+  const [state,setState] = useState(0);
+
   return (
-    <div>App</div>
+    <>
+    <CounterContext.Provider value={{state,setState}}>
+       APP 
+       <CompA/>
+      {/* <button onClick={()=>setState(state+1)}>Click here to increase</button> */}
+    </CounterContext.Provider>
+    </>
   )
 }
 
-export default App
+export default App;
+export {CounterContext}
